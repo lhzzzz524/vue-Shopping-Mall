d09ab7773ab9519ec42af5e0561bbf4963ebc42b
hzl <Hangzhe.Lv@b-and-qchina.com>
2021-10-25 14:20:24 +0800

    test

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="tuku">
-    <daohanglan :title="['效果图首页', '家具美图', '精美图册']"></daohanglan>
-    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import MainTabBar from "./test/fenlei/fenleiitempc.vue";
-import Daohanglan from "./test/daohanglan.vue";
 export default {
   data() {
     return {};
